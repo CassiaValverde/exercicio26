7fd44233f1a74c42e2a7d13c361a1854841f693a
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-10-16 21:02:17 -0300

    Add files via upload

---
+++ exercicio26.py
@@ -0,0 +1,5 @@
+a = float(input('insira o primeiro valor \n'))
+b = float(input('insira o segundo valor \n'))
+c = float(input('insira o terceiro valor \n'))
+calculo = a+b+c
+print(f'O valor total de sua compra é {calculo}')
